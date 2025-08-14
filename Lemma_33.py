d051d3fb0094fb5d5e1b45cbad21ad8ff99971f4
Dimitris Spathoulas <147658216+DimSpathoulas@users.noreply.github.com>
2025-08-14 12:05:13 +0300

    Update Lemma_33.py

--- Lemma_33.py
+++ Lemma_33.py
@@ -1,3 +1,5 @@
+# Spathoulas Dimitris
+
 # ### NOT YET IMPLEMENTED
 # We adopt a self-balancing binary search tree
 # (e.g. Red-Black Tree [GS78]) to dynamically maintain these upper bounds, with O(max{1, log(N/M )})
